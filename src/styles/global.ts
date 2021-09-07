6eae3c998863b67073908f48965d59cdab0690ae
Jônatas Livramento <jonatas.livramento@gmail.com>
2021-09-07 01:10:44 -0300

    Criado Dashboard e filhos

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,6 +4,7 @@ export const GlobalStyle = createGlobalStyle`
   :root {
     --background: #f0f2f5;
     --red: #e52e4D;
+    --green: #33cc95;
     --blue: #5429cc;
     --blue-light: #6933ff;
     --text-title: #363f5f;
@@ -36,7 +37,7 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smooth: antialiased;
   }
 
