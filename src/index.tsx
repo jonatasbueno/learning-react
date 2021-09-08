464f02e768bc6cc3d1236382fb0921dbb8d29768
Jônatas Livramento <jonatas.livramento@gmail.com>
2021-09-08 00:53:03 -0300

    Configurado miragejs (mock server)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { createServer } from 'miragejs';
+
+/**
+ * Criado mock server com miragejs
+ */
+createServer({
+  routes() {
+    this.namespace = 'api'; // basepoint que será interceptado
+
+    this.get('/transactions', () => { // endpoint e seu respectivo retorno
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
