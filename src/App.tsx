3dd3b6fe224d72bd2a75c3858e662ad5049b9ca9
Jônatas Livramento <jonatas.livramento@gmail.com>
2021-09-09 05:19:20 -0300

    Criado modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
-import Modal from 'react-modal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-
-Modal.setAppElement('#root'); // isso inseri aria-hidden na div#root quando modal estiver aberto (acessibilidade)
 
 export function App() {
   const [isNewTransactionModal, setIsNewTransactionModal] = useState(false);
@@ -21,12 +19,10 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModal}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-      </Modal>
+      />
       <GlobalStyle />
     </>
   );
