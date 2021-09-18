16c59e8bb858eb7dc38847027b8ae833effedac7
Jônatas  Bueno do Livramento <jonatas.livramento@MacBook-Pro.local>
2021-09-17 21:17:39 -0300

    Criado model 'transaction' no schema do miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 
 /**
  * Criado mock server com miragejs
  */
 createServer({
+  models: {
+    transaction: Model, // define uma entidade dentro do database (screma)
+  },
+
   routes() {
     this.namespace = 'api'; // basepoint que será interceptado
 
     this.get('/transactions', () => { // endpoint e seu respectivo retorno
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction'); // retorna todas as transactions do database (schema)
+    })
+
+    this.post('/transaction', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transcation', data); // cria uma transaction no database (schema)
     })
   }
 })
